dd4f6d01eaec50693717990532f166a3523d2fe9
Andres Marti <martia01@students.zhaw.ch>
2021-12-10 17:41:03 +0100

    first functional version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Person } from './person';
 import { PersonService } from './person.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,8 @@ import { PersonService } from './person.service';
 })
 export class AppComponent implements OnInit {
   public persons: Person[];
+  public editPerson: Person | undefined;
+  public deletePerson: Person | undefined;
 
   constructor(private personService: PersonService){
     this.persons = [];
@@ -29,5 +32,82 @@ export class AppComponent implements OnInit {
         alert(error.message);
       }
     );
+  }
+
+  public onAddPerson(addForm: NgForm): void {
+    //document.getElementById('add-person-form').click();
+    this.personService.addPerson(addForm.value).subscribe(
+      (response: Person) => {
+        console.log(response);
+        this.getPersons();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdatePerson(person: Person): void {
+    this.personService.updatePerson(person).subscribe(
+      (response: Person) => {
+        console.log(response);
+        this.getPersons();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeletePerson(personId: number): void {
+    this.personService.deletePerson(personId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getPersons();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public searchPerson(key: string): void {
+    console.log(key);
+    const results: Person[] = [];
+    for (const person of this.persons) {
+      if (person.vorname.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || person.nachname.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || person.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || person.mobil.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(person);
+      }
+    }
+    this.persons = results;
+    if (results.length === 0 || !key) {
+      this.getPersons();
+    }
+  }
+
+  public onOpenModal(mode: string, person?: Person): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button'
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add') {
+      button.setAttribute('data-target', '#addPersonModal')
+    }
+    else if(mode === 'edit') {
+      this.editPerson = person;
+      button.setAttribute('data-target', '#updatePersonModal')
+    }
+    else if(mode === 'delete') {
+      this.deletePerson = person;
+      button.setAttribute('data-target', '#deletePersonModal')
+    }
+    container?.appendChild(button);
+    button.click();
   }
 }
